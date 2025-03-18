c01b8ff18075c7d0e98be507406fa7fe86f0695e
eli6 <elin.fritiofsson@protonmail.com>
2025-03-18 17:18:29 +0100

    easier to suggest services

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,8 @@ export default function RootLayout({
         <Script
           data-goatcounter="https://aiatlas.goatcounter.com/count"
           async
-            src="//gc.zgo.at/count.js"
-          />
+          src="//gc.zgo.at/count.js"
+        />
       </body>
     </html>
   );
